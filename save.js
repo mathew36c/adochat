d8b5cbe9a3a122ab7c770671ad99c5d1aec22dfc
Mathew Oscar Garalde <mathewogaralde@gmail.com>
2016-10-20 16:23:59 +0000

    new

---
+++ save.js
@@ -0,0 +1,25 @@
+$('#savebtn').click(function() {
+    var saveit = $('#calendar').fullCalendar( 'clientEvents');
+    var eventsholded = [];
+
+    $.each(saveit, function(index,value) {
+        var event = new Object();
+        event.id = value.id;            
+        event.start = value.start;
+        event.end = value.end;
+        event.title = value.title;
+        event.allDay = value.allDay
+        eventsholded.push(event);
+    }); 
+    
+    $.ajax
+    ({
+        type: "GET",
+        dataType : 'json',
+        async: false,
+        url: 'http://your.host/save_json.php',
+        data: { data: JSON.stringify(eventsholded) },
+        success: function () {alert("Thanks!"); },
+        failure: function() {alert("Error!");}
+    });
+});
